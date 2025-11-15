e7040a6cd89bc0cc6d455af4cd25232720e387cc
hemanth <heamdanger@gmail.com>
2025-11-15 13:38:40 +0530

    last leetcode question

--- leetcodetrials.py
+++ leetcodetrials.py
@@ -1,25 +1,7 @@
-dividend=1
-divisor=2
-if (dividend>0 and divisor >0) or (dividend<0 and divisor<0):
-    reminder=dividend-divisor
-    if abs(reminder)>=abs(divisor):
-        q=1
-    else: q=0
-    while abs(reminder)>=abs(divisor):
-        q+=1
-        dividend=reminder
-        reminder=dividend-divisor
-    print (q)
-elif dividend<0 or divisor<0:
-    reminder=dividend+divisor
-    if abs(reminder)>=abs(divisor):
-        q=-1
-    else: q=0
-    while abs(reminder)>=abs(divisor):
-        q-=1
-        dividend=reminder
-        reminder=dividend+divisor
-    print(q)
+s="hemanth"
+s=list(s)
+s=s[::-1]
+print(s)
 
            
 
